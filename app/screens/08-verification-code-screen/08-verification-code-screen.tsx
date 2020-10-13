3eb97796fcc1e89e5566f599985d608deb8e2658
sonseven5997 <sonseven5997@gmail.com>
2020-10-13 22:14:19 +0700

    verify screen & set language screen

--- app/screens/08-verification-code-screen/08-verification-code-screen.tsx
+++ app/screens/08-verification-code-screen/08-verification-code-screen.tsx
@@ -1,14 +1,76 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../components"
+import { Screen, Text, Button } from "../../components"
+import {View, TextInput} from 'react-native'
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+
+  firstText: {
+    width: 250,
+    height: 106,
+  },
+  firstTextContent: {
+    fontFamily: 'SegoeUI-Bold',
+    fontSize: 40,
+    color: 'rgb(0,0,0)',
+    fontWeight: 'bold',
+  },
+  secondText:{
+    marginBottom: 40
+  },
+  secondTextContent: {
+    fontFamily: 'SegoeUI-Regular',
+    fontSize: 17,
+    color: 'rgb(102,102,102)'
+  },
+  thirdTextContent: {
+    fontSize: 17,
+    fontFamily: 'SegoeUI-Regular',
+    color: 'rgb(138,138,143)'
+  },
+  codeVerifyWrapper:{
+    marginTop: 8,
+    marginBottom: 36,
+
+  },
+  codeVerify: {
+    fontSize: 17,
+    fontFamily: 'SegoeUI-Regular',
+    color: 'rgb(0,0,0)'
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    borderRadius: 8,
+    backgroundColor: 'rgb(147,194,47)',
+  },
+  lastTextContentWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  lastTextContent:{
+    fontSize: 17,
+    fontFamily: 'SegoeUI-Regular',
+    color: 'rgb(138,138,143)'
+  },
+  buttonText: {
+    fontSize: 17,
+    fontFamily: 'SegoeUI-Regular',
+    color: 'rgb(255,255,255)'
+  }
+})
 
 const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
+  backgroundColor: color.palette.white,
   flex: 1,
+  marginHorizontal: 32,
+  marginTop: 80,
 }
 
 export const VerificationCodeScreen = observer(function VerificationCodeScreen() {
@@ -22,6 +84,21 @@ export const VerificationCodeScreen = observer(function VerificationCodeScreen()
   return (
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" text="VerificationCodeScreen" />
+      <View style={styles.firstText}>
+        <Text style={styles.firstTextContent}>Xác thực lại số của bạn</Text>
+      </View>
+      <View style={styles.secondText}>
+        <Text style={styles.secondTextContent}>Chúng tôi sẽ gửi mã xác thực tới số mobile </Text>
+      </View>
+      <Text style={styles.thirdTextContent}>Nhập mã xác thực</Text>
+      <View style={styles.codeVerifyWrapper}>
+        <TextInput style={styles.codeVerify}></TextInput>
+      </View>
+      <Button text="Xác thực" style={styles.button} textStyle={styles.buttonText}></Button>
+      <View style={styles.lastTextContentWrapper}>
+        <Text style={styles.lastTextContent}>Gửi lại mã xác thực</Text>
+        <Text style={styles.lastTextContent}>1:20 phút còn lại</Text>
+      </View>
     </Screen>
   )
 })
--- app/screens/11-set-language-screen/11-set-language-screen.tsx
+++ app/screens/11-set-language-screen/11-set-language-screen.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../components"
+import { ViewStyle, Text, ImageBackground, CheckBox, View } from "react-native"
+import { Screen, Button } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
 
 const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
+  backgroundColor: color.palette.white,
   flex: 1,
 }
 
@@ -22,6 +22,27 @@ export const SetLanguageScreen = observer(function SetLanguageScreen() {
   return (
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" text="SetLanguageScreen" />
+      <ImageBackground></ImageBackground>
+      <Text>Hi Thuý!</Text>
+      <Text>Please select your preferred language to facilitate communication</Text>
+      <View>
+        <CheckBox /> <Text>English</Text>
+        <CheckBox /> <Text>Chinese</Text>
+      </View>
+      <View>
+        <CheckBox /> <Text>Portuguese</Text>
+        <CheckBox /> <Text>Spanish</Text>
+      </View>
+      <View>
+        <CheckBox /> <Text>Hindi</Text>
+        <CheckBox /> <Text>Arabic</Text>
+        <CheckBox /> <Text>Russian</Text>
+      </View>
+      <View>
+        <CheckBox /> <Text>Bulgarian</Text>
+        <CheckBox /> <Text>Lithuanian</Text>
+      </View>
+      <Button text='Select'></Button>
     </Screen>
   )
 })
