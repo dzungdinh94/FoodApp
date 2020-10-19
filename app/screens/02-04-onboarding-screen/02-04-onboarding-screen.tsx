4cd14c8b16fedece1bc9fb3796b9637ec21ef553
ngmanhquan2000 <ngmanhquan2000@gmail.com>
2020-10-19 22:14:28 +0700

    19/10

--- app/screens/02-04-onboarding-screen/02-04-onboarding-screen.tsx
+++ app/screens/02-04-onboarding-screen/02-04-onboarding-screen.tsx
@@ -1,16 +1,33 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { ViewStyle, StyleSheet } from "react-native"
 import { Screen, Text } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
 import { Checkbox } from "react-native-paper"
-
+import LinearGradient from 'react-native-linear-gradient';
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
 }
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+});
 
 export const OnboardingScreen = observer(function OnboardingScreen() {
   // Pull in one of our MST stores
@@ -24,7 +41,11 @@ export const OnboardingScreen = observer(function OnboardingScreen() {
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" text="OnboardingScreen" />
       <Text text="Tìm cửa hàng gần nhất" />
-
+      <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
+        <Text style={styles.buttonText}>
+          Sign in with Facebook
+        </Text>
+      </LinearGradient>
     </Screen>
   )
 })
