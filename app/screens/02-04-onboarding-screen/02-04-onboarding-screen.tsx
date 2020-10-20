489af1f930fba8411df0ba436a1717a4d87ccf71
ngmanhquan2000 <ngmanhquan2000@gmail.com>
2020-10-20 22:17:55 +0700

    '20/10'

--- app/screens/02-04-onboarding-screen/02-04-onboarding-screen.tsx
+++ app/screens/02-04-onboarding-screen/02-04-onboarding-screen.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, StyleSheet, View, } from "react-native"
-import { Screen, Text, Button } from "../../components"
+import { ViewStyle, StyleSheet, View, Image, Linking } from "react-native"
+import { Screen, Text, Button, Wallpaper } from "../../components"
+import OnBoardingCarousel from "../../components/OnBoardingCarousel/OnBoardingCarousel"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
@@ -16,17 +17,21 @@ import Hyperlink from 'react-native-hyperlink'
 const ROOT: ViewStyle = {
   backgroundColor: "#C8C7CC",
   flex: 1,
+  // flexDirection: 'row',
+  minHeight: 734,
+  minWidth: 375
 }
 
 const styles = StyleSheet.create({
   FormButton: {
-    flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    marginTop: 94,
+    
   },
   ButtonSignUp: {
     backgroundColor: "#93C22F",
+    flex: 1,
     height: 50,
-    width: 150,
     borderColor: "#93C22F",
     borderWidth: 1,
     borderRadius: 8,
@@ -35,11 +40,12 @@ const styles = StyleSheet.create({
   },
   ButtonSignIn: {
     backgroundColor: "transparent",
+    flex: 1,
     height: 50,
-    width: 150,
     borderColor: "#FFFFFF",
     borderWidth: 1,
     borderRadius: 8,
+    marginRight: 32
   },
   TextStyle: {
     fontSize: 17,
@@ -48,6 +54,16 @@ const styles = StyleSheet.create({
   linearGradient: {
     flex: 1,
   },
+  Img: {
+    position: "relative",
+    marginLeft: 60,
+    marginTop: 92,
+  },
+  Formtext: {
+    marginLeft: 32,
+    marginTop: 40,
+    marginRight: 32,
+  }
 })
 export const OnboardingScreen = observer(function OnboardingScreen() {
   // Pull in one of our MST stores
@@ -64,20 +80,27 @@ export const OnboardingScreen = observer(function OnboardingScreen() {
       <LinearGradient
         style={styles.linearGradient}
         colors={['#00000000', '#000000']}>
-        <Text preset="header" text="OnboardingScreen" />
+        <Image source={require('/MindX/FoodApp/app/picture/logo-removebg-preview.png')} style={styles.Img} />
+        <OnBoardingCarousel></OnBoardingCarousel>
         <View style={styles.FormButton}>
           <Button textStyle={styles.TextStyle} style={styles.ButtonSignUp} text="Đăng kí" onPress={() => navigation.navigate("SignUpScreen")} />
           <Button textStyle={styles.TextStyle} style={styles.ButtonSignIn} text="Đăng nhập" onPress={() => navigation.navigate("SignInScreen")} />
         </View>
-        <View>
-        <Hyperlink
-          linkStyle={{ color: '#2980b9', fontSize: 12,  }}
-          linkText={url => url === 'https://github.com/obipawan/hyperlink' ? 'Hyperlink' : url}
-        >
-          <Text style={{ fontSize: 13 }}>
-            Make clickable strings cleaner with https://github.com/obipawan/hyperlink
-    </Text>
-        </Hyperlink>
+        <View style = {styles.Formtext}>
+          <Text style={{textAlign: 'center',fontWeight: '100'}}>
+            <Text style ={{fontSize: 13}}>
+            &nbsp; Bằng cách đồng ý và gia nhập {" "}
+            </Text>
+            <Text style={{fontSize: 13, color: '#C8C7CC', fontWeight:'bold'}}
+              onPress={() => Linking.openURL('http://google.com')}>
+              Điều khoản sử dụng 
+            </Text>
+            <Text style ={{fontSize: 13, fontWeight: '100'}}> và &nbsp;</Text>
+            <Text style={{fontSize: 13, color: '#C8C7CC',fontWeight:'bold' }}
+              onPress={() => Linking.openURL('http://google.com')}>
+              Chính sách bảo mật
+            </Text>
+          </Text>
         </View>
       </LinearGradient>
     </Screen>
